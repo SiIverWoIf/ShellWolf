7417ff852acd6901e8ea60b953a8dd1479d1359b
SiIverWoIf <simonbence869@gmail.com>
2025-02-11 23:53:20 +0100

    [any message]

--- src/main.cs
+++ src/main.cs
@@ -7,11 +7,20 @@ while(true)
 {
     Console.Write("$ ");
     string userInput = Console.ReadLine();
-    if (userInput.StartsWith("exit"))
+    if (userInput.StartsWith("exit "))
     {
         string[] h = userInput.Split();
         int exitStatus = Convert.ToInt32(h[1]);
         return exitStatus;
+    }
+    if (userInput.StartsWith("echo "))
+    {
+        string[] h = userInput.Split();
+        for (int i = 1; i < h.Length; i++)
+        {
+            Console.Write(h[i] + " ");
+        }
+        Console.WriteLine();
     }
     else
     {
