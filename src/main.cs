1c8ae0a19bdf70b89a924bfe91ad39a60e53ddd3
SiIverWoIf <simonbence869@gmail.com>
2025-02-19 17:03:12 +0100

    [any message]

--- src/main.cs
+++ src/main.cs
@@ -47,7 +47,7 @@ while(true)
             string[] path = Environment.GetEnvironmentVariable("path").Split(';');
             for (int i = 0; i < path.Length; i++)
             {
-                if (File.Exists($"{path[i]}\\{h[1]}.*"))
+                if (File.Exists($"{path[i]}\\{h[1]}"))
                 {
                     Console.WriteLine($"{h[1]} is {path[i]}\\{h[1]}");
                     isFound = true;
