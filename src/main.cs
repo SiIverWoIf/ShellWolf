bf8afe26537ba101783f62053ac17271d65bd939
SiIverWoIf <simonbence869@gmail.com>
2025-02-19 16:49:40 +0100

    [any message]

--- src/main.cs
+++ src/main.cs
@@ -1,5 +1,6 @@
 #pragma warning disable CS8600
 #pragma warning disable CS8602
+
 using System.Globalization;
 using System.Net;
 using System.Net.Sockets;
@@ -7,8 +8,10 @@ using System.Net.Sockets;
 while(true)
 {
     string[] builtinCommands = ["exit", "echo", "type"];
+
     Console.Write("$ ");
     string userInput = Console.ReadLine();
+
     if (userInput.StartsWith("exit "))
     {
         string[] h = userInput.Split();
@@ -36,8 +39,34 @@ while(true)
                 break;
             }
         }
-        if (isBuiltin) Console.WriteLine($"{h[1]} is a shell builtin");
-        else Console.WriteLine($"{h[1]}: not found");
+        if (isBuiltin)
+            Console.WriteLine($"{h[1]} is a shell builtin");
+        else if (OperatingSystem.IsWindows())
+        {
+            string[] path = Environment.GetEnvironmentVariable("path").Split(';');
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (File.Exists($"{i}\\{h[1]}.*"))
+                {
+                    Console.WriteLine($"{h[1]} is {i}\\{h[1]}");
+                    break;
+                }
+            }
+        }
+        else if (OperatingSystem.IsLinux())
+        {
+            string[] path = Environment.GetEnvironmentVariable("path").Split(':');
+            for (int i = 0; i < path.Length; i++)
+            {
+                if (File.Exists($"{i}/{h[1]}"))
+                {
+                    Console.WriteLine($"{h[1]} is {i}/{h[1]}");
+                    break;
+                }
+            }
+        }
+        else 
+            Console.WriteLine($"{h[1]}: not found");
     }
     else
     {
